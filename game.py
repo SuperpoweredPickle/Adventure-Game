04e2044b4905528c1bb5cf61f5d6c54343a821ee
SuperpoweredPickle <25StaponaiteV@smrt.bristol.sch.uk>
2026-03-22 16:23:25 +0000

    Update game.py

--- game.py
+++ game.py
@@ -1,19 +1,21 @@
 #intoduction
-Name = input("Enter your name! ")
-print(f"Greetings {Name} Welcome to your adventure! ")
+def introduction():
+    Name = input("Enter your name! ")
+    print(f"Greetings {Name} Welcome to your adventure! ")
+introduction()
 
 start = input ("Would you rather play the game or perish >:( ")
 if start == 'play':
     print ("Great! Let's play!")
     setting = input("Want to go to the Jungle, Desert or Tundra? ")
-else:  
+else:
     print("Lame. Okay you're existence was wiped off the earth now")
     quit()
-#jungle ending 1/3
+
 if setting == "jungle":
     print ("Welcome to the mighty jungle! Your tour guide told you to wait here.... ")
-    response = input("But he left to get ice-cream. Follow him or wait here? ") 
-    
+    response = input("But he left to get ice-cream. Follow him or wait here? ")
+
     if response == "Follow":
         print ("You follow him into the trees and stumble upon a family of gorillas. ")
         response = input("Do you fight them or back away or leave them be? ")
@@ -26,7 +28,8 @@ if setting == "jungle":
         #ending 1/3
     else:
         print ("Wrong answer, You lose. ")
-        quit() 
+        quit()
+
 if setting == "desert":
     print ("Welcome to the Sahara Desert! Your tour guide told you to wait here.... ")
     response = input("But he left to get ice-cream. Follow him or wait here? ")
@@ -38,6 +41,7 @@ if setting == "desert":
     else:
         print ("Wrong answer, You lose. ")
         quit()
+        
 if setting == "tundra":
     print ('Welcome to the Tundra! Your tour guide turned into a human lolly! ')
     response = input ("Do you unfreeze him or abandon him? ")
